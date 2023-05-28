6cad97d0dbb792ed9764819dbbdb263d3b9a8ca3
Carlos7979 <carloscorredorwg@gmail.com>
2023-05-28 19:00:43 -0400

    add cookie-parser module

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const cookieParser = require('cookie-parser')
 const morgan = require('morgan')
 
 // handlebars
@@ -12,6 +13,7 @@ app.set('view engine', 'handlebars')
 // express
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
 app.use(morgan('dev'))
 app.use('/static', express.static('public'))
 
